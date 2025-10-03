eecce75d68530bdd77ec0a55b539826daf277b06
Ching Him Leung <cleung@jlab.org>
2025-10-03 16:23:58 -0400

    more pre-defined paths
    add shellbeg

--- dnf.sh
+++ dnf.sh
@@ -1,3 +1,4 @@
+#! /bin/bash
 dnf update -y 
 dnf install -y 'dnf-command(config-manager)'
 dnf config-manager --set-enabled crb
--- root.sh
+++ root.sh
@@ -1,3 +1,4 @@
+#! /bin/bash
 cd /opt
 ROOT_BIN=root_v6.36.02.Linux-almalinux9.6-x86_64-gcc11.5.tar.gz
 curl -O https://root.cern/download/${ROOT_BIN}
